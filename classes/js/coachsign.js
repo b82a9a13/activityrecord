a7a4bf09184f2ccca56cfda86fd56320afe11c71
b82a9a13 <85174237+b82a9a13@users.noreply.github.com>
2023-08-04 15:23:57 +0100

    Delete classes/js directory

--- classes/js/coachsign.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('coach_signed_btn').addEventListener('click', ()=>{
-    document.getElementById('coach_signed_btn').style.display = 'none';
-    document.getElementById('coachsigndate').style.display = 'block';
-})
--- classes/js/learnsign.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('learn_signed_btn').addEventListener('click', ()=>{
-    document.getElementById('learn_signed_btn').style.display = 'none';
-    document.getElementById('learnsigndate').style.display = 'block';
-})
